20d772eb6cc6b9f6af39e1b1736e1aef23b43798
Oleg Nepiuk <gtailde@gmail.com>
2023-07-13 23:38:26 +0200

    add: Added basic page layout

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
 import "./style.css";
+
+/*      Car run function     */
+
+// const carIcon = <HTMLDivElement>document.querySelector('.car-icon');
+// const endPosition = window.innerWidth - window.innerHeight * 0.25;
+// let currentPosition = 0;
+
+// function moveCar() {
+//   if (currentPosition >= endPosition) {
+//     clearInterval(intervalId);
+//     return;
+//   }
+
+//   currentPosition += 5;
+//   carIcon.style.transform = `translateX(${currentPosition}px)`;
+// }
+
+// const intervalId = setInterval(moveCar, 10);
